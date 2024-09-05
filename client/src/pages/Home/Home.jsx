45e1c21e5777635804a203c3b91dc83a97a12da9
chintuRAJamrit <133254549+chintuRAJamrit@users.noreply.github.com>
2024-09-05 23:20:20 +0530

    init

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Leftsidebar from '../../Component/Leftsidebar/Leftsidebar'
+import Rightsidebar from '../../Component/Rightsidebar/Rightsidebar'
+import Homemainbar from '../../Component/Homemainbar/Homemainbar'
+import '../../App.css'
+const Home = ({slidein}) => {
+  return (
+    <div className="home-container-1">
+      <Leftsidebar slidein={slidein}/>
+      <div className="home-container-2">
+        <Homemainbar/>
+        <Rightsidebar/>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/pages/Question/Displayquestion.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Leftsidebar from '../../Component/Leftsidebar/Leftsidebar'
+import Rightsidebar from '../../Component/Rightsidebar/Rightsidebar'
+import Questiondetails from './Questiondetails.jsx'
+
+const Displayquestion = ({slidein}) => {
+  return (
+    <div className="home-container-1">
+      <Leftsidebar slidein={slidein}/>
+      <div className="home-container-2">
+        <Questiondetails/>
+        <Rightsidebar/>
+      </div>
+    </div>
+  )
+}
+
+export default Displayquestion
